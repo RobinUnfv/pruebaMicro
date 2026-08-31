0cee257c673f336f584aebbf073bdd14b440b7e8
RobinPC <sant4n4.1@gmail.com>
2026-08-31 17:38:24 -0500

    Se configuro el client oauth2

--- client-oauth2/src/main/java/com/robin/client_oauth2/config/SecurityConfig.java
+++ client-oauth2/src/main/java/com/robin/client_oauth2/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.robin.client_oauth2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,11 +11,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
       return http.authorizeHttpRequests( authorizeRequets ->
               authorizeRequets.requestMatchers("/public").permitAll()
                       .anyRequest().authenticated()
-      ).build();
+      ).oauth2Login(Customizer.withDefaults()).build();
 
     }
 
--- client-oauth2/src/main/java/com/robin/client_oauth2/expone/web/ClientController.java
+++ client-oauth2/src/main/java/com/robin/client_oauth2/expone/web/ClientController.java
@@ -1,5 +1,7 @@
 package com.robin.client_oauth2.expone.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,16 @@ public class ClientController {
     }
 
     @GetMapping("/protected")
-    public String protectedResource() {
-        return "Protected Resource";
+    public String protectedResource(@AuthenticationPrincipal OAuth2User principal) {
+
+        if (principal == null) {
+            throw new RuntimeException("User is not authenticated");
+        }
+
+        String username = principal.getAttribute("name");
+        System.out.println("Username: " + username);
+
+        return String.format("Hello %s!", username);
     }
 
 }
